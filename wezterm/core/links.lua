a4904b5a7f5add8263d0f485a107fdc88fcabc85
CoolPlayLin <CoolPlayLin@qq.com>
2023-12-23 12:10:56 +0800

    feat(wezterm): add link rules :sparkles:

--- wezterm/core/links.lua
+++ wezterm/core/links.lua
@@ -9,6 +9,18 @@ function links_config.setup(config)
       regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
       format = "mailto:$0"
     },
+    {
+      regex = "\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b",
+      format = "$0"
+    },
+    {
+      regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
+      format = "$0"
+    },
+     {
+      regex = [[\bfile://\S*\b]],
+      format = "$0",
+    }
   }
 end
 
